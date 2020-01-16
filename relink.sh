d1c082dac1e8f9c13424efb226a68595294f2c6a
David Nix <hello@davidnix.io>
2020-01-16 16:40:23 -0700

    Add and reconfigure for neovim

--- relink.sh
+++ relink.sh
@@ -4,4 +4,11 @@ set -e
 
 echo "Generating links.."
 find $PWD -name ".[^.]*" -type f -print0 | xargs -0tJ % ln -sf %  ~
+
+# Vim setup
+mkdir -p ~/.vim
+ln -sf $PWD/.vim/.vimrc ~/.vim/.vimrc
+mkdir -p ~/.config/nvim
+ln -sf $PWD/.config/nvim/init.vim ~/.config/nvim/init.vim
+
 echo "Complete!"
