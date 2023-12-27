f2fe89fa00b4cf32ad0dd1aadebf24cfba53c05c
David Nix <hello@davidnix.io>
2023-12-27 15:21:41 -0700

    Move snippets out of init.lua

---
+++ .config/nvim/lua/config/snippets.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+M.setup = function()
+  -- Great tutorial: https://www.youtube.com/watch?v=FmHhonPjvvA
+  local ls = require("luasnip")
+  local s = ls.snippet
+  local t = ls.text_node
+  local i = ls.insert_node
+  local fmt = require("luasnip.extras.fmt").fmt
+
+  -- My custom go snippets
+  ls.add_snippets("go", {
+    -- Require no error
+    s("nerr", {
+      t("require.NoError(t, err)"),
+    }),
+    -- Require error
+    s("rerr", {
+      t({ "require.Error(t, err)", 'require.Error(t, err, "changeme")' }),
+    }),
+    -- Table driven test
+    s(
+      "ttable",
+      fmt(
+        [[
+    for _, tt := range []struct {{
+      {}
+    }}{{
+      {{}},
+    }} {{
+    }}
+  ]],
+        { i(1) }
+      )
+    ),
+  })
+end
+
+return M
