6ed25968584a6b8cdf60927177030b3f2ce9663d
David Nix <hello@davidnix.io>
2023-12-26 20:53:28 -0700

    Add toggle go test file

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -13,8 +13,26 @@ map("i", "jk", "<esc>l", defaults)
 -- Map command to ;
 map("n", ";", ":", { noremap = true })
 
--- Remove trailing space
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*" },
-  command = [[%s/\s\+$//e]],
-})
+-- Go test files
+function ToggleGoTestFile()
+  local current_file = vim.fn.expand("%")
+
+  if vim.fn.expand("%:e") == "go" then
+    if vim.fn.match(current_file, "_test.go$") >= 0 then
+      -- It's a test file, find the corresponding source file
+      local source_file = vim.fn.substitute(current_file, "_test.go$", ".go", "")
+      vim.api.nvim_command("edit " .. source_file)
+    else
+      -- It's a source file, find or create the corresponding test file
+      local test_file = vim.fn.substitute(current_file, ".go$", "_test.go", "")
+      if vim.fn.filereadable(test_file) == 0 then
+        -- Create the test file if it doesn't exist
+        vim.fn.system("gotests -w -exported " .. vim.fn.shellescape(current_file))
+      end
+      vim.api.nvim_command("edit " .. test_file)
+    end
+  else
+    print("Not a Go file")
+  end
+end
+map("n", "<leader>gt", ":lua ToggleGoTestFile()<CR>", defaults)
