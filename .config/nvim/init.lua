5c419ce0108312a9ef2252bcdf621936ad01c133
David Nix <hello@davidnix.io>
2023-12-27 13:12:03 -0700

    Create go lua snippets

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,37 @@ require("config.lazy")
 
 -- Auto-closing pairs is annoying
 vim.g.minipairs_disable = true
+
+-- Snippets
+-- Great tutorial: https://www.youtube.com/watch?v=FmHhonPjvvA
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+ls.add_snippets("go", {
+  -- Require no error
+  s("nerr", {
+    t("require.NoError(t, err)"),
+  }),
+  -- Require error
+  s("rerr", {
+    t({ "require.Error(t, err)", 'require.Error(t, err, "changeme")' }),
+  }),
+  -- Table driven test
+  s(
+    "ttable",
+    fmt(
+      [[
+    for _, tt := range []struct {{
+      {}
+    }}{{
+      {{}},
+    }} {{
+    }}
+  ]],
+      { i(1) }
+    )
+  ),
+})
