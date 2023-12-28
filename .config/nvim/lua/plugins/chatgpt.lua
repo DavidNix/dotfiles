f27dc580dd5719ea72c93762a8342b2720f1910f
David Nix <hello@davidnix.io>
2023-12-28 09:50:02 -0700

    Add ChatGPT plugin

---
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -0,0 +1,33 @@
+local model = "gpt-4-1106-preview"
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    -- For options see: https://github.com/jackMort/ChatGPT.nvim/blob/main/lua/chatgpt/config.lua
+    require("chatgpt").setup({
+      api_key_cmd = "op read op://Personal/OpenAINeovim/password --no-newline",
+      openai_params = {
+        model = model,
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = 300,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+      openai_edit_params = {
+        model = model,
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+    })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+}
